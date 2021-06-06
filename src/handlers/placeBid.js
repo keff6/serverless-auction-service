b1055fda473b6a4b891b6a7ead36b67dc596dc28
keff6 <kev.fallas@gmail.com>
2021-06-06 17:47:15 -0600

    Add authorization

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,8 +10,17 @@ const dynamodb = new AWS.DynamoDB.DocumentClient()
 async function placeBid(event, context) {
   const { id } = event.pathParameters
   const { amount } = event.body
+  const { email } = event.requestContext.authorizer
 
   const auction = await getAuctionById(id)
+
+  if(auction.seller === email) {
+    throw new createError.Forbidden(`You are the seller`)
+  }
+
+  if(auction.highestBid.bidder === email) {
+    throw new createError.Forbidden(`You are the highest bidder, you can't bit now`)
+  }
 
   if(auction.status === 'CLOSED') {
     throw new createError.Forbidden('You cannot bid on closed auctions')
@@ -21,12 +30,15 @@ async function placeBid(event, context) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
   }
 
+  
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   }
